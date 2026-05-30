c99801cbfd1e8a4ce06fd46dfeab8fb25604b123
Avinash519-chauhan <ac9969160@gmail.com>
2026-05-30 19:04:06 +0530

    Done

--- Weather.js
+++ Weather.js
@@ -1,4 +1,4 @@
-let apikey = "ecb6a458ce820d15f2c1b677435aa5ad"
+let apikey = "ecb6a458ce820d15f2c1b677435aa5ad";
 
 let searchBtn = document.querySelector("#search-btn")
 let resultBox = document.querySelector("#result")
@@ -14,7 +14,48 @@ searchBtn.addEventListener("click",async ()=>{
     }
 
     try {
+        resultBox.classList.remove("hidden");
+
+        resultBox.innerHTML = `
+        <div class="flex justify-center items-center">
+            <div class="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin">
+                
+            </div>
+            <p class="text-center mt-3 text-gray-700 font-semibold">Fetching Weather Data...</p>
+        </div>`;
+
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`)
         
+        let data = await response.json()
+        console.log(data);
+
+        if(!response.ok){
+            resultBox.innerHTML = `<h3>City Not Found...</h3>`;
+            return;
+        }
+        
+        //Weather icon
+        let icon = data.weather[0].icon;
+
+        resultBox.innerHTML = 
+        `<div>
+            <h2 class="text-xl font-bold text-gray-600">${data.name}, ${data.sys.country}</h2>
+            <p class="text-gray-600 mt-1">${data.weather[0].main}</p>
+            <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="loading..." class="mx-auto"/>
+            <h1 class="text-5xl font-bold text-blue-600">${data.main.temp}°C</h1>
+        </div>
+        
+        <div class="grid grid-cols-2 gap-4 mt-6">
+        <div class="bg-white/60 p-4 rounded-xl shadow">
+        <p class="text-gray-600 text-sm">Humidity</p>
+        <h3 class="text-xl fond-bold text-gray-800">${data.main.humidity}%</h3>
+        </div>
+
+        <div class="bg-white/60 p-4 rounded-xl shadow">
+        <p class="text-gray-600 text-sm">Wind Speed</p>
+        <h3 class="text-xl fond-bold text-gray-800">${data.wind.speed}%</h3>
+        </div>
+        </div>`;
     } catch (error) {
         console.log(error);
         resultBox.innerHTML = `<h3>Error in Fetching Weather Data...</h3>`;
